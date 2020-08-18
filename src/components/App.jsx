ac2dc4bef92e09805407ead38e5bb3a90a1d2b05
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-18 09:54:14 -0400

    useState for mouseOver

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello!");
+  const [isMouseOver, setMouseOver] = useState(false);
 
   function handleClick() {
     setHeadingText("Submitted");
   }
 
-  function handleMouseOver() {}
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
 
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
       <button
-        style={{ backgroundColor: "white" }}
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
         onClick={handleClick}
         onMouseOver={handleMouseOver}
       >
